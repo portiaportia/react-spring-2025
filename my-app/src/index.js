425df1bb846fb81dfd9504bd49022586e9b4bf24
portiaportia <portiaplante@gmail.com>
2025-03-25 09:46:07 -0400

    updated

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,11 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {BrowserRouter, Routes, Route} from "react-router-dom";
 import './index.css';
+import Layout from './Layout';
+import Home from './Home';
+import Blog from './Blog';
 import Nopage from './pages/Nopage';
+
+function App() {
+  return(
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="blog" element={<Blog />} />
+          <Route path="*" element={<Nopage/>} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Nopage />
+    <App />
   </React.StrictMode>
 );
