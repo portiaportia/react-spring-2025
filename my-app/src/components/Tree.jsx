df9080779602320b476ea46d4280a338b8e27762
portiaportia <portiaplante@gmail.com>
2025-04-01 08:53:29 -0400

    pruning

--- my-app/src/components/Tree.jsx
+++ my-app/src/components/Tree.jsx
@@ -1,11 +1,19 @@
 import "./css/Tree.css";
+import {useState} from "react";
 
 function Tree(props) {
+    const [pruned, pruneTree] = useState(false);
+
+    const doTreeJob = () => {
+        pruneTree(!pruned);
+    };
+    
     return (
         <section className="tree">
-            <h3>{props.name}</h3>
+            <h3 className={pruned?"done":""}>{props.name}</h3>
             <p>{props.description}</p>
             <img src={process.env.PUBLIC_URL + "/" + props.image} />
+            <p><button onClick ={doTreeJob}>Prune This Tree</button></p>
         </section>
     );
 }
