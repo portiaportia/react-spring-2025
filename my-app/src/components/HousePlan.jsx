18af511df5a42051987146b1cd91787f120fbd43
portiaportia <portiaplante@gmail.com>
2025-04-01 10:45:44 -0400

    updated house plan

--- my-app/src/components/HousePlan.jsx
+++ my-app/src/components/HousePlan.jsx
@@ -2,8 +2,16 @@ import "./css/HousePlan.css";
 
 const HousePlan = (props) => {
     return (
-        <section className="house-plan">
-            <h3>{props.name}</h3>
+        <section className="house-plan columns">
+            <section className="feature-image">
+                <img src={"http://portiaportia.github.io/json/images/house-plans/" + props.main_image} alt={props.name}/>
+            </section>
+            <section className = "info">
+                <h3>{props.name}</h3>
+                <p>Size: {props.size}</p>
+                <p>Bedrooms: {props.bedrooms}</p>
+                <p>Bathrooms: {props.bathrooms}</p>
+            </section>
         </section>
     );
 };
--- my-app/src/components/HousePlans.jsx
+++ my-app/src/components/HousePlans.jsx
@@ -3,8 +3,13 @@ import HousePlan from "./HousePlan";
 const HousePlans = () => {
     return (
         <div id="house-plans" className="columns">
-            <HousePlan 
-                name="Best House" />
+            <HousePlan
+                _id="1"
+                name="Farm House" 
+                size="2000"
+                bedrooms="3"
+                bathrooms="2.5"
+                main_image="farm.webp"/>
         </div>
     );
 };
