b25190f8648673c2107070abded8fe202c716e2c
portiaportia <portiaplante@gmail.com>
2025-04-01 09:04:46 -0400

    udat

--- my-app/src/components/Navigation.jsx
+++ my-app/src/components/Navigation.jsx
@@ -1,11 +1,19 @@
 import {Outlet, Link} from "react-router-dom";
+import {useState} from "react";
 import './css/Navigation.css';
 
 
 function Navigation() {
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+    }
+
     return (
         <nav id="main-nav">
-            <ul>
+            <button onClick={toggleMenu}>...</button>
+            <ul className="hide-small">
                 <li><Link to="/">Home</Link></li>
                 <li><Link to="/about">About</Link></li>
                 <li><Link to="/blog">Blog</Link></li>
