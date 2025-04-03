290dd1c6e889a8ec8a56ffa851b1166002538170
portiaportia <portiaplante@gmail.com>
2025-04-03 08:49:39 -0400

    updated house plnas

--- my-app/src/components/HousePlans.jsx
+++ my-app/src/components/HousePlans.jsx
@@ -1,15 +1,23 @@
+import {useState} from "react";
+import axios from "axios";
 import HousePlan from "./HousePlan";
+
 //https://portiaportia.github.io/json/house-plans.json
 const HousePlans = () => {
+    const [houses, setHouses] = useState([]);
+
     return (
         <div id="house-plans" className="columns">
-            <HousePlan
+            {houses.map((house)=>(
+                <HousePlan
                 _id="1"
-                name="Farm House" 
-                size="2000"
-                bedrooms="3"
-                bathrooms="2.5"
-                main_image="farm.webp"/>
+                name={house.name}
+                size={house.size}
+                bedrooms={house.bedrooms}
+                bathrooms={house.bathrooms}
+                main_image={house.main_image}/>
+            ))}
+            
         </div>
     );
 };
