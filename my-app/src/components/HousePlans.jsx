996cf7bad8a5bf1413697c40f7395135161e4cc8
portiaportia <portiaplante@gmail.com>
2025-04-03 08:56:00 -0400

    adding axios

--- my-app/src/components/HousePlans.jsx
+++ my-app/src/components/HousePlans.jsx
@@ -1,10 +1,19 @@
-import {useState} from "react";
+import {useState, useEffect} from "react";
 import axios from "axios";
 import HousePlan from "./HousePlan";
 
 //https://portiaportia.github.io/json/house-plans.json
 const HousePlans = () => {
     const [houses, setHouses] = useState([]);
+
+    //after page loaded to asynch json retrieval
+    useEffect(()=>{
+        //automatically execute the async function
+        (async () => {
+            const response = await axios.get("https://portiaportia.github.io/json/house-plans.json");
+        })();
+
+    },[]);
 
     return (
         <div id="house-plans" className="columns">
